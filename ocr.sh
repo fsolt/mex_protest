49d9d0e15d6212b83cd4c6bba73cd05edb411477
Frederick Solt <fsolt@mac.com>
2015-02-11 12:33:56 -0600

    ocr.sh: cleaned up and final

--- ocr.sh
+++ ocr.sh
@@ -6,21 +6,11 @@
 PREFIX=$(basename "$1" .pdf)
 echo "Current document: $PREFIX"
 echo "Converting to TIFF . . ."
-if command -v parallel >/dev/null 2>&1; then
-  LAST_PAGE=$(($(xpdf-pdfinfo "$1"|grep '^Pages:'|awk '{print $2}') - 1))
-  parallel --bar -u convert -density 300 "$1"\[\{1\}\] -type Grayscale -compress lzw -background white +matte -depth 32 "${PREFIX}_page_%05d.tif" ::: $(seq 0 $LAST_PAGE)
-else
-  convert -density 300 "$1" -type Grayscale -compress lzw -background white +matte -depth 32 "${PREFIX}_page_%05d.tif"
-fi
+LAST_PAGE=$(($(xpdf-pdfinfo "$1"|grep '^Pages:'|awk '{print $2}') - 1))
+parallel --bar -u convert -density 300 "$1"\[\{1\}\] -type Grayscale -compress lzw -background white +matte -depth 32 "${PREFIX}_page_%05d.tif" ::: $(seq 0 $LAST_PAGE)
+
 echo "Performing OCR . . ."
-if command -v parallel >/dev/null 2>&1; then
-  parallel --bar -u --retries 3 "tesseract -l spa {} {.} pdf 2>/dev/null" ::: "${PREFIX}"_page_*.tif
-else
-  for i in "${PREFIX}"_page_*.tif; do
-    echo $i
-    tesseract -l spa "$i" "$(basename "$i" .tif)" pdf 2>/dev/null
-  done
-fi
+parallel --bar -u --retries 3 "tesseract -l spa {} {.} pdf 2>/dev/null" ::: "${PREFIX}"_page_*.tif
 
 echo "Cleaning up . . ."
 cat "${PREFIX}"_page_*.txt > "${PREFIX}".txt
